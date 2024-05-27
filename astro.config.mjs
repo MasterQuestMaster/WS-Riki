1501b8bf8ceea64b516a2b520850f841dbbcc201
Manuel Hoffmann <manuel.hoffmann.yt@gmail.com>
2024-05-27 21:32:33 +0200

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
-
 import db from "@astrojs/db";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://wsriki.com",
-  integrations: [icon(), robotsTxt(), db()]
+  integrations: [icon(), robotsTxt(), db()],
+  adapter: cloudflare()
 });
