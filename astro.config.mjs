4c9d0d2a7f5603f3fae313249dbf04d3a3ea9ed3
Manuel Hoffmann <manuel.hoffmann.yt@gmail.com>
2024-05-29 23:36:06 +0200

    cloudflare adapter test

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   output: "server",
   site: "https://wsriki.com",
   integrations: [icon(), robotsTxt(), db()],
-  adapter: node({
+  adapter: cloudflare() /* node({
     mode: "standalone"
-  })
+  })*/
 });
