9c69dbca40fb43cc615f54126886b07c763b7330
Manuel Hoffmann <manuel.hoffmann.yt@gmail.com>
2024-05-29 23:22:57 +0200

    first css + update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import db from "@astrojs/db";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://wsriki.com",
   integrations: [icon(), robotsTxt(), db()],
-  adapter: cloudflare()
+  adapter: node({
+    mode: "standalone"
+  })
 });
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
 import presetEnv from "postcss-preset-env"
+import autoprefixer from "autoprefixer"
+import cssnanoPlugin from "cssnano"
 
 export const plugins = [
-    presetEnv({stage:0})
+    presetEnv({stage:0}),
+    autoprefixer(),
+    cssnanoPlugin({
+        preset: 'default',
+    })
 ]
