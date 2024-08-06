7c2651390e8e09f977ebdcf5ce7bed5645532022
Manuel Hoffmann <manuel.hoffmann.yt@gmail.com>
2024-08-06 20:52:49 +0200

    Revert "wrangler setup (for env vars)"
    This reverts commit 84e5eba548f7fbaf2827c5229b2a69bc577234d8.
    
    test without wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 import db from "@astrojs/db";
 import cloudflare from "@astrojs/cloudflare";
-import icon from "astro-icon";
 import scope from "astro-scope";
-import robotsTxt from "astro-robots-txt";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://wsriki.com",
   integrations: [icon(), robotsTxt(), db(), scope()],
-  adapter: cloudflare({
-    imageService: "passthrough",
-    platformProxy: {
-      enabled: true
-    }
-  })
+  adapter: cloudflare() /* node({
+    mode: "standalone"
+  })*/
 });
